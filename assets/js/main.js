ddb4e7d35dfd32343606605f54783a96a859667c
Gabriel Calcagni <93176365+solidsnk86@users.noreply.github.com>
2023-08-01 05:35:51 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -335,28 +335,104 @@ function compartirLinkedIn() {
                   ------> IMAGE CARD <-------
  -------------------------------------------------------------*/
 
-// Función para mostrar el modal con la imagen grande cuando se hace clic en un elemento portfolio-item
-   function showLargeImage(event) {
-     const modalImage = document.getElementById('modal-image');
-     const largeImageUrl = event.target.src;
- 
-// Mostrar el modal y establecer la imagen grande en el modal
-     modalImage.src = largeImageUrl;
-     document.querySelector('.modal-overlay').style.display = 'block';
-   }
- 
-// Agregar evento de clic a todos los elementos con la clase 'portfolio-item'
-   const portfolioItems = document.querySelectorAll('.portfolio-item');
-   portfolioItems.forEach(item => {
-     item.addEventListener('click', showLargeImage);
-   });
- 
-// Agregar evento de clic para cerrar el modal cuando se hace clic fuera de la imagen
-   document.querySelector('.modal-overlay').addEventListener('click', (event) => {
-     if (event.target === event.currentTarget) {
-       document.querySelector('.modal-overlay').style.display = 'none';
-     }
-   });
+ /**
+   * Initiate glightbox
+   */
+ const glightbox = GLightbox({
+  selector: '.glightbox'
+});
+
+/**
+ * Porfolio isotope and filter
+ */
+let portfolionIsotope = document.querySelector('.portfolio-isotope');
+
+if (portfolionIsotope) {
+
+  let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
+  let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
+  let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
+
+  window.addEventListener('load', () => {
+    let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
+      itemSelector: '.portfolio-item',
+      layoutMode: portfolioLayout,
+      filter: portfolioFilter,
+      sortBy: portfolioSort
+    });
+
+    let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
+    menuFilters.forEach(function(el) {
+      el.addEventListener('click', function() {
+        document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
+        this.classList.add('filter-active');
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        if (typeof aos_init === 'function') {
+          aos_init();
+        }
+      }, false);
+    });
+
+  });
+
+}
+
+/**
+ * Init swiper slider with 1 slide at once in desktop view
+ */
+new Swiper('.slides-1', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+/**
+ * Init swiper slider with 2 slides at once in desktop view
+ */
+new Swiper('.slides-2', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
+});
   
 
 
